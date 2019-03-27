4f7cee462fb09bd7cbbc81c66cc7b091acdcc2d6
matrayu <northridge614@gmail.com>
2019-03-27 05:40:32 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SearchParams from './components/SearchParams/SearchParams'
+import Data from './Data'
+
 import './App.css';
+
+const staticBooks = Data.store.items
+console.log(staticBooks)
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Google Book Search</h1>
+          <SearchParams />
         </header>
       </div>
     );
