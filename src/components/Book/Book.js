b9df9e50d7979b7b719eb40f4d6a7b5d1bb68b99
matrayu <northridge614@gmail.com>
2019-03-27 05:40:41 -0700

    Create Book.js

---
+++ src/components/Book/Book.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Book.css';
+
+class Book extends Component {
+  render() {
+    return (
+      <div className="book">
+      </div>
+    );
+  }
+}
+
+export default Book;
