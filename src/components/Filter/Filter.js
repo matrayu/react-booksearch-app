bae74da3059ddb17499184f5af3e9a83ab9b0d63
matrayu <northridge614@gmail.com>
2019-03-28 06:56:33 -0700

    Update Filter.js
    Updated Filter to handle filtering options and push data up to state

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 import './Filter.css';
 
 class Filter extends Component {
+  handlePrintType = (e) => {
+    this.filterPrintType(e.target.value)
+  };
+
+  filterPrintType = (printType) => {
+    this.props.handlePrintType(printType)
+  };
+
+  handleBookType = (e) => {
+    this.filterBookType(e.target.value)
+  };
+
+  filterBookType = (bookType) => {
+    this.props.handleBookType(bookType)
+  };
+
   render() {
     return (
       <div className="filter">
         <form className="filter__form">
             <label>Print Type:
-                <select>
+                <select defaultValue={this.props.printType} onChange={this.handlePrintType}>
                     <option value="all">All</option>
                     <option value="books">Books</option>
                     <option value="magazines">Magazines</option>
                 </select>    
             </label> 
             <label>Book Type: 
-                <select>
+                <select defaultValue={this.props.bookType} onChange={this.handleBookType}>
                     <option value="no-filter">No Filter</option>
                     <option value="ebooks">eBooks</option>
                     <option value="free-ebooks">Free eBooks</option>
