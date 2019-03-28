bd285c980b9ba71e793cabd5c5474dbbb2a643f6
matrayu <northridge614@gmail.com>
2019-03-28 06:56:59 -0700

    Update Search.js
    Adding functionality to push search term up to state

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import './Search.css';
 
 class Search extends Component {
+  handleSearch = (e) => {
+    this.handleSubmit(e.target.value)
+  };
+
+  handleSubmit = (searchTerm) => {
+    this.props.handleSearch(searchTerm)
+  };
+
+  
   render() {
     return (
       <div className="search">
         <form className="search__form">
             <label htmlFor="search">Search:</label>
-            <input type="text" name="search" id="search" placeholder="Shantaram"/>
+            <input type="text" name="search" id="search" placeholder="Shantaram" value={this.props.searchTerm} onChange={this.handleSearch} />
         </form>
       </div>
     );
