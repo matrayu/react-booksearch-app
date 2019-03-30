6010212f0a6ede2a723061ad6570badc661f59da
matrayu <northridge614@gmail.com>
2019-03-30 08:19:11 -0700

    Update Book.js

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -3,23 +3,25 @@ import './Book.css';
 
 class Book extends Component {
   render() {
-    const priceFormat = this.props.book.price
-      ? new Intl.NumberFormat(`${this.props.book.country}`, { style: 'currency', currency: `${this.props.book.currencyCode}` }).format(this.props.book.price)
+    const {book} = this.props;
+
+    const priceFormat = book.price
+      ? new Intl.NumberFormat(`${book.country}`, { style: 'currency', currency: `${book.currencyCode}` }).format(book.price)
       : 'Not For Sale';
 
     return (
       <div className="book">
         <div className="book__row">
-          <h2 className="book__title">{this.props.book.title}</h2>
+          <h2 className="book__title">{book.title}</h2>
         </div>
         <div className="book__details">
           <div className="book__image">
-            <img src={this.props.book.thumbnail} title={this.props.book.title} alt='No Img Available' />
+            <img src={book.thumbnail} title={book.title} alt='No Img Available' />
           </div>
           <div className="book__subDetails">
-            <div className="book__author">Author: {this.props.book.author}</div>
+            <div className="book__author">Author: {book.author}</div>
             <div className="book__price">Price: {priceFormat}</div>
-            <div className="book__description">{this.props.book.shortDescription}</div>
+            <div className="book__description">{book.shortDescription}</div>
           </div>
         </div>
       </div>
