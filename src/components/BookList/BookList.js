14d980667d3f938b0571b08f5d1a456f861673c5
matrayu <northridge614@gmail.com>
2019-03-29 19:40:29 -0700

    Update BookList.js

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -3,26 +3,28 @@ import Book from '../Book/Book'
 import './BookList.css';
 
 class BookList extends Component {
+
   render() {
-    console.log(this.props.bookList)
-    const books = this.props.bookList.map((book, i) => {
-      const bookObj = {
-        key:{i},
-        title: book.volumeInfo.title,
-        isEbook: book.saleInfo.isEbook,
-        description: book.volumeInfo.description,
-        shortDescription: book.searchInfo.textSnippet,
-        thumbnail: book.volumeInfo.imageLinks.thumbnail,
-        author: book.volumeInfo.authors,
-        country: book.saleInfo.country,
-        /* price: 10.00,
-        currencyCode: 'USD', */
-        price: book.saleInfo.listPrice.amount,
-        currencyCode: book.saleInfo.listPrice.currencyCode,
-        saleability: book.saleInfo.saleability,
-      }
-      return <Book book={bookObj} key={i} />;
-    });
+    let books = ''
+    if(this.props.bookList) {
+      books = this.props.bookList.map((book, i) => {
+        const bookObj = {
+          title: book.volumeInfo.title,
+          isEbook: book.saleInfo.isEbook,
+          description: book.volumeInfo.description,
+          shortDescription: book.searchInfo ? book.searchInfo.textSnippet : '',
+          thumbnail: book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : '',
+          author: book.volumeInfo.authors,
+          country: book.saleInfo.country,
+          price: book.saleInfo.listPrice ? book.saleInfo.listPrice.amount : '',
+          currencyCode: book.saleInfo.listPrice ? book.saleInfo.listPrice.currencyCode : '',
+          saleability: book.saleInfo.saleability,
+        }
+        return <Book book={bookObj} key={i} />;
+      })
+    } else {
+      books = `Your search - ${this.props.searchTerm} - did not match any book results.`
+    }
 
     return (
       <div className="bookList">
